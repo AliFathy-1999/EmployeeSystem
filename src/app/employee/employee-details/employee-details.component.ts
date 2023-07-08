7991f53ed4a8ef9230c7ac170547dbcccfcf17cb
AliFathy-1999 <ali_ahmed_1999@hotmail.com>
2023-07-08 14:57:46 +0300

    Refactor add employee and add update btn to details

--- src/app/employee/employee-details/employee-details.component.ts
+++ src/app/employee/employee-details/employee-details.component.ts
@@ -9,9 +9,36 @@ import { GlobalService } from 'src/app/service/global.service';
 })
 export class EmployeeDetailsComponent implements OnInit{
   empId:any;
-  employeeData:any;
+  employeeData = {
+    _id:'',
+    firstName: '',
+    lastName: '',
+    nationalId: '',
+    DOB: '',
+    gender: '',
+    academicQualifications: {
+      college: '',
+      degree: '',
+      institution: '',
+      year: ''
+    },
+    hireDate: '',
+    position: '',
+    jobType: '',
+    depId:{ _id:'',name:''},
+    salary: '',
+    phoneNumber: '',
+    address: '',
+    pImage:''
+  };
+  userRole:any;
+  isAdmin:boolean = false;
   constructor(private _global:GlobalService,public route:ActivatedRoute){
     this.empId = this.route.snapshot.params['id'];
+    this.userRole = this._global.currentUser["_value"].role
+    if(this.userRole == "ADMIN"){
+      this.isAdmin = true;
+    }
   }
   ngOnInit(): void {
     this._global.getEmployeeDetails(this.empId).subscribe((data:any) => {
